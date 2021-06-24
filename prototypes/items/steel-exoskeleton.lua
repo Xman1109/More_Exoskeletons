4c1fa29efe8493e3ae2a330670d49707c2acf34c
Xman1109 <shawnoliver1109@gmail.com>
2021-06-24 22:06:40 +0200

    initial commit

---
+++ prototypes/items/steel-exoskeleton.lua
@@ -0,0 +1,20 @@
+data:extende({
+    
+    {
+        type = "movement-bonus-equipment",
+        name = "steel-exoskeleton",
+        categories = { "armor", "spidertron-only" },
+        icon = "__More_Exoskeletons__/graphics/item/steel-exoskeleton.png"
+        energy_consumption = "300kW",
+        energy_source = {
+          type = "electric",
+          usage_priority = "secondary-input",
+        },
+        movement_bonus = 0.4,
+        shape = {
+          width = 2,
+          height = 4,
+          type = "full",
+        }
+    }
+})
